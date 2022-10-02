af76c6c40a2b8491fd26d468dc25581ee586ce0c
NMishchenko <mishchenko.nls@gmail.com>
2022-10-02 16:48:27 +0300

    Add room switching

---
+++ sun-runner-unity/Assets/Scripts/Navigation/Navigation.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Navigation : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        gameObject.GetComponent<Image>().color = new Color(255, 255, 255, 0.5f);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        gameObject.GetComponent<Image>().color = new Color(255, 255, 255, 0);
+    }
+}
